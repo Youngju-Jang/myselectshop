82155abaaed183242a247c926e1fddeb220c4f9f
Youngju Jang <joj1043@nate.com>
2022-12-03 12:27:23 +0900

    8. 중복 폴더 생성 이슈 해결하기

--- src/main/java/com/sparta/myselectshop/service/FolderService.java
+++ src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -52,12 +52,16 @@ public class FolderService {
                User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
                     () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
                );
-               
+               List<Folder> existFolderList = folderRepository.findAllByUserAndNameIn(user, folderNames);
+     
                List<Folder> folderList = new ArrayList<>();
-               
+     
                for (String folderName : folderNames) {
-                    Folder folder = new Folder(folderName, user);
-                    folderList.add(folder);
+                    // 이미 생성한 폴더가 아닌 경우만 폴더 생성
+                    if (!isExistFolderName(folderName, existFolderList)) {
+                         Folder folder = new Folder(folderName, user);
+                         folderList.add(folder);
+                    }
                }
                
                return folderRepository.saveAll(folderList);
@@ -128,5 +132,15 @@ public class FolderService {
           } else {
                return null;
           }
+     }
+     private boolean isExistFolderName(String folderName, List<Folder> existFolderList) {
+          // 기존 폴더 리스트에서 folder name 이 있는지?
+          for (Folder existFolder : existFolderList) {
+               if (existFolder.getName().equals(folderName)) {
+                    return true;
+               }
+          }
+          
+          return false;
      }
 }
