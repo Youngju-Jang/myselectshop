485540753f1b542b8800f4b6e4dfb88a6e0ee84e
Youngju Jang <joj1043@nate.com>
2022-12-03 11:05:55 +0900

    5. 폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface  ProductRepository  extends JpaRepository<Product, Long> {
-     List<Product> findAllByUserId(Long userId, Pageable pageable);
+     Page<Product> findAllByUserId(Long userId, Pageable pageable);
      Optional<Product> findByIdAndUserId(Long id, Long userId);
+     Page<Product> findAll(Pageable pageable);
 }
